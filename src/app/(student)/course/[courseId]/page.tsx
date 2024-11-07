50bb59079bce79c9ef2f941bd83e1968f238b0c0
shub-saini <anshsaini04@gmail.com>
2024-11-07 15:26:53 +0530

    updated /course/{id}

--- src/app/(student)/course/[courseId]/page.tsx
+++ src/app/(student)/course/[courseId]/page.tsx
@@ -1,5 +1,6 @@
 import { HtmlContent } from "@/components/html-content";
 import { PrismaClient } from "@prisma/client";
+import Image from "next/image";
 import { redirect } from "next/navigation";
 
 export default async function Course({
@@ -16,23 +17,30 @@ export default async function Course({
     },
   });
 
-  console.log(course);
-
   if (!course) {
     return redirect("/browse");
     return <div className="mt-32 text-4xl">Course not found</div>;
   }
 
   return (
-    <div className="flex flex-col gap-8 mt-24">
+    <div className="flex flex-col gap-8 mt-24 px-7">
       <div className="flex items-center gap-4">
         <div>
-          <h1 className="text-3xl font-bold">{course.name}</h1>
-          <p className="text-gray-500">{course.creator.name}</p>
+          <h1 className="text-3xl font-bold mb-6">{course.name}</h1>
+          <Image
+            src={course.thumbnailUrl ?? ""}
+            alt="course thumbnail"
+            height={600}
+            width={600}
+          />
+          <div className="mt-6">Price: ₹{course.price}</div>
         </div>
       </div>
 
-      <HtmlContent html={course.about ?? ""} />
+      <div>
+        <div className="text-slate-500">Description: </div>
+        <HtmlContent html={course.about ?? ""} className="text-xl" />
+      </div>
     </div>
   );
 }
